9ae85c5d4428458201234def973a9cf2b5f3805a
iarzt <58914841+iarzt@users.noreply.github.com>
2020-06-26 23:19:17 -0500

    Add files via upload

---
+++ active_spin.sql
@@ -0,0 +1,2 @@
+SELECT active_spin_fastball, pitch_movement.full_name, rise, pitcher_break_z, percent_rank_diff_z FROM `active-spin`
+RIGHT JOIN pitch_movement ON pitch_movement.full_name = `active-spin`.full_name
