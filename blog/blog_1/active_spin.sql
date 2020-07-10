e36a8f17891ef9419cd21b3cfa5dbbbcfb69d8e0
iarzt <58914841+iarzt@users.noreply.github.com>
2020-07-10 15:29:55 -0500

    Update active_spin.sql

--- blog/blog_1/active_spin.sql
+++ blog/blog_1/active_spin.sql
@@ -1,3 +1,3 @@
 SELECT active_spin_fastball, pitch_movement.full_name, rise, pitcher_break_z, percent_rank_diff_z 
 FROM `active-spin`
-RIGHT JOIN pitch_movement ON pitch_movement.full_name = `active-spin`.full_name
+RIGHT JOIN pitch_movement ON pitch_movement.full_name = `active-spin`.full_name;
