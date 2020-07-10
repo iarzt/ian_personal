17a26c5c49c4c8a2c53124db8171ff0e687b51a1
iarzt <58914841+iarzt@users.noreply.github.com>
2020-07-10 15:27:52 -0500

    Update active_comp.sql

--- blog/blog_1/active_comp.sql
+++ blog/blog_1/active_comp.sql
@@ -1,2 +1,3 @@
-SELECT `active-spin`.full_name, `active-spin`.active_spin_fastball, active2018.active_spin_fastball FROM `active-spin`
+SELECT `active-spin`.full_name, `active-spin`.active_spin_fastball, active2018.active_spin_fastball 
+FROM `active-spin`
 RIGHT JOIN active2018 ON `active-spin`.full_name_code = active2018.full_name
