564e1c47f0869ee4d8ae21e3f06d08562b42a5d8
iarzt <58914841+iarzt@users.noreply.github.com>
2020-07-10 15:29:18 -0500

    Update active_whiff.sql

--- blog/blog_1/active_whiff.sql
+++ blog/blog_1/active_whiff.sql
@@ -1,3 +1,6 @@
-SELECT Name, active_spin_fastball, MPH, `Whiff %` FROM scrape
+SELECT Name, active_spin_fastball, MPH, `Whiff %` 
+FROM scrape
 RIGHT JOIN `active-spin` ON full_name_code = scrape.Name
-WHERE scrape.Year = '2019' AND `Pitch Type` = 'Four Seamer' AND scrape.`#` > 200;
+WHERE scrape.Year = '2019' 
+AND `Pitch Type` = 'Four Seamer' 
+AND scrape.`#` > 200;
