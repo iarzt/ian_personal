d070dac3c5ebc96918587517d4371aa7d0f07925
iarzt <58914841+iarzt@users.noreply.github.com>
2020-07-10 15:27:29 -0500

    Update quick_look.sql

--- blog/blog_1/quick_look.sql
+++ blog/blog_1/quick_look.sql
@@ -1,4 +1,5 @@
 SELECT player_name1, active_spin_fastball, EV, LA XBA, XSLG, XWOBA
 FROM active_spin_examples
 LEFT JOIN `active-spin` ON player_name1 = full_name
-WHERE time = '2019' AND pitch_type = 'Four Seamer';
+WHERE time = '2019' 
+AND pitch_type = 'Four Seamer';
